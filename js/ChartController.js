0b92869fbf71b13358e26c8651ed5c46e78008b9
Unknown <alexander.micklow@gmail.com>
2017-12-14 11:42:08 -0600

    Bar charts added
    Added bar charts, with labels, with data read in from external .json. Made some small changes to styling.

--- js/ChartController.js
+++ js/ChartController.js
@@ -1,5 +1,5 @@
 app.controller('ChartController', ['$scope', function ($scope) {
-
+    //styles the svg tabs at the top of the oage
     $scope.initTabs = function () {
         var svg = d3.select("body");
 
@@ -16,7 +16,7 @@ app.controller('ChartController', ['$scope', function ($scope) {
                     .attr("fill", "MidnightBlue");
             });
     };
-
+    //draws a line chart and two bar charts
     $scope.initChart = function () {
         var margin = {
                 top: 20,
@@ -43,7 +43,7 @@ app.controller('ChartController', ['$scope', function ($scope) {
                 return y(d.value);
             });
 
-        var svg = d3.select("body").append("svg")
+        var line = d3.select("body").append("svg")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
             .append("g")
@@ -74,17 +74,17 @@ app.controller('ChartController', ['$scope', function ($scope) {
             })]);
 
             // Add the valueline path.
-            svg.append("path")
+            line.append("path")
                 .data([data])
                 .attr("class", "line")
                 .attr("d", valueline);
             // Add the X Axis
-            svg.append("g")
+            line.append("g")
                 .attr("transform", "translate(0," + height + ")")
                 .call(d3.axisBottom(x));
 
             // Add the Y Axis
-            svg.append("g")
+            line.append("g")
                 .call(d3.axisLeft(y));
         }
         // Get the data
@@ -94,12 +94,16 @@ app.controller('ChartController', ['$scope', function ($scope) {
             // trigger render
             draw(data, "data");
         });
-
+        //parse json
         d3.json("data/dataCat.json", function (error, data) {
             if (error) throw error;
+            //extract data
             data = data.data;
+
+            //initialize filter of data
             var cf = crossfilter(data);
 
+            //initialize dimensions
             var colorDimension = cf.dimension(function (d) {
                 return d.category1;
             });
@@ -108,9 +112,11 @@ app.controller('ChartController', ['$scope', function ($scope) {
                 return d.category2;
             });
 
+            //initialize groupings
             var colorGrouping = colorDimension.group();
             var alphaGrouping = alphaDimension.group();
 
+            //initialize reduce functions
             function reduceAdd(p, v) {
                 return p + v.value;
             }
@@ -122,38 +128,134 @@ app.controller('ChartController', ['$scope', function ($scope) {
             function reduceInitial() {
                 return 0;
             }
-            /*
-            for (var i = 0; i < data.length; i++) {
-                if (!(colors.includes(data[i].category1))) {
-                    colors.push(data[i].category1);
-                }
-            }
 
-            for (i = 0; i < data.length; i++) {
-                if (!(alphas.includes(data[i].category2))) {
-                    alphas.push(data[i].category2);
-                }
-            }
-
-            for (i = 0; i < colors.length; i++) {
-                colors[i] = {
-                    "color": colors[i],
-                    "value": 0
-                };
-            }
-            
-            for (i = 0; i < alphas.length; i++) {
-                alphas[i] = {
-                    "alpha": alphas[i],
-                    "value": 0
-                };
-            }
-            */
+            //reduce groupings
             colorGrouping.reduce(reduceAdd, reduceRemove, reduceInitial);
             alphaGrouping.reduce(reduceAdd, reduceRemove, reduceInitial);
-            
+
+            //extract arrays of grouped data
             var colors = colorGrouping.all();
             var alphas = alphaGrouping.all();
+
+            //put data into arrays
+            var colorVals = [];
+            for (var i = 0; i < colors.length; i++) {
+                colorVals.push(colors[i].value);
+            }
+            var alphaVals = [];
+            for (i = 0; i < alphas.length; i++) {
+                alphaVals.push(alphas[i].value);
+            }
+
+            //initialize bar chart scales
+            var w = 300;
+            var h = 300;
+            
+            //initialize pads
+            var chartGap = 4;
+            var textIndent = 18;
+
+            var xScale = d3.scaleBand()
+                .domain(d3.range(alphaVals.length))
+                .rangeRound([0, w])
+                .paddingInner(0.05);
+            var yScale = d3.scaleLinear()
+                .domain([0, d3.max([d3.max(colorVals), d3.max(alphaVals)])])
+                .range([0, h]);
+
+            //initialize bar chart svg elements
+            var bar1 = d3.select("body")
+                .append("svg")
+                .attr("float", "right")
+                .attr("width", w + chartGap)
+                .attr("height", h);
+
+            var bar2 = d3.select("body")
+                .append("svg")
+                .attr("width", w)
+                .attr("height", h);
+
+            //initalize bars
+            bar1.selectAll("rect")
+                .data(colors)
+                .enter()
+                .append("rect")
+                .attr("x", function (d, i) {
+                    return xScale(i);
+                })
+                .attr("y", function (d) {
+                    return h - yScale(d.value);
+                })
+                .attr("width", xScale.bandwidth())
+                .attr("height", function (d) {
+                    return yScale(d.value);
+                })
+                .attr("fill", function (d) {
+                    return d.key;
+                })
+                .append("title")
+                .text(function (d) {
+                    return d.key;
+                });
+
+            bar2.selectAll("rect")
+                .data(alphas)
+                .enter()
+                .append("rect")
+                .attr("x", function (d, i) {
+                    return xScale(i);
+                })
+                .attr("y", function (d) {
+                    return h - yScale(d.value);
+                })
+                .attr("width", xScale.bandwidth())
+                .attr("height", function (d) {
+                    return yScale(d.value);
+                })
+                .attr("fill", function (d) {
+                    return "rgb(0, 0, " + Math.round(d.value / 2) + ")";
+                })
+                .append("title")
+                .text(function (d) {
+                    return d.key;
+                });
+
+            //append number labels
+            bar1.selectAll("text")
+                .data(colors)
+                .enter()
+                .append("text")
+                .text(function (d) {
+                    return d.value;
+                })
+                .attr("text-anchor", "middle")
+                .attr("x", function (d, i) {
+                    return xScale(i) + xScale.bandwidth() / 2;
+                })
+                .attr("y", function (d) {
+                    return h - yScale(d.value) + textIndent;
+                })
+                .attr("font-family", "sans-serif")
+                .attr("font-size", "12px")
+                .attr("fill", "black");
+
+            bar2.selectAll("text")
+                .data(alphas)
+                .enter()
+                .append("text")
+                .text(function (d) {
+                    return d.value;
+                })
+                .attr("text-anchor", "middle")
+                .attr("x", function (d, i) {
+                    return xScale(i) + xScale.bandwidth() / 2;
+                })
+                .attr("y", function (d) {
+                    return h - yScale(d.value) + textIndent;
+                })
+                .attr("font-family", "sans-serif")
+                .attr("font-size", "12px")
+                .attr("fill", "white");
 
         });
     }
