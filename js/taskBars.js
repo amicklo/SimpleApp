74a915099bcb128e6b00bd995442b55096f880c0
Unknown <alexander.micklow@gmail.com>
2018-01-10 16:06:19 -0600

    Mostly Style Changes
    Bars are colored depending upon their status (a newly added parameter); Y-axis labels are larger

--- js/taskBars.js
+++ js/taskBars.js
@@ -2,28 +2,53 @@ app.factory('taskBars', [function () {
     return {
         //attach bars
         addBars: function (svg, data, x, y, barOffset, labelOffset, fill, start, end) {
+
+            var colorIndex = {
+                Complete: "#20AA20",
+                Progress: "#0000CC",
+                Overdue: "#DD0000"
+            };
+
             svg.selectAll(".chart")
+                .append("g")
+                .attr("id", "bars")
                 .data(data)
                 .enter()
                 .append("rect")
-                .attr("fill", fill)
+                .attr("fill", function (d) {
+                    if (start == "startActual") {
+                        return colorIndex[d.status];
+                    }
+                    return fill;
+                })
                 .attr("rx", 5)
                 .attr("ry", 5)
                 .attr("y", 0)
                 .attr("transform", function (d) {
                     return "translate(" + x(moment(d[start])) + "," + (y(d.person) + barOffset) + ")";
                 })
-                .attr("height", 45)
+                .on("mouseover", function () {
+                    d3.select(this)
+                        .attr("stroke", "yellow");
+                })
+                .on("mouseout", function () {
+                    d3.select(this)
+                        .attr("stroke", "none");
+                })
+                .attr("height", 35)
                 .attr("width", function (d) {
                     return (x(moment(d[end])) - x(moment(d[start])));
-                })
+                });
             //attach labels to the bars
             svg.selectAll(".chart")
+                .append("g")
+                .attr("id", "labels")
                 .data(data)
                 .enter()
                 .append("text")
                 .attr("fill", "white")
                 .attr("font-size", "13px")
+                .attr("font-weight", "600")
                 .attr("y", 0)
                 .attr("transform", function (d) {
                     return "translate(" + (x(moment(d[start])) + 5) + "," + (y(d.person) + labelOffset) + ")";
--- js/tasks.js
+++ js/tasks.js
@@ -18,11 +18,8 @@ app.directive('tasks', ['taskBars', function (taskBars, $scope) {
                     left: 150,
                 }
                 //initialize time domains
-                var dayOne = "2018-01-01 03:00:00",
-                    dayTwo = "2018-01-02 03:00:00";
-
-                var timeDomainStart = d3.timeDay.offset(moment(dayOne), +0),
-                    timeDomainEnd = d3.timeHour.offset(moment(dayTwo), +0);
+                var timeDomainStart = d3.timeDay.offset(moment("2018-01-01 03:00:00"), +0),
+                    timeDomainEnd = d3.timeHour.offset(moment("2018-01-02 03:00:00"), +0);
                 //create an array of individuals from the JSON
                 var people = [];
                 for (var i in data) {
@@ -31,7 +28,8 @@ app.directive('tasks', ['taskBars', function (taskBars, $scope) {
                     }
                 }
                 //initialize the dimensions of the chart
-                var height = (120 * people.length),
+                var rowHeight = 120,
+                    height = (rowHeight * people.length),
                     width = 1000;
                 //format string for the time labels of the x-axis
                 var tickFormat = "%H:%M";
@@ -44,23 +42,6 @@ app.directive('tasks', ['taskBars', function (taskBars, $scope) {
                 var y = d3.scaleBand()
                     .domain(people)
                     .rangeRound([0, height - margin.bottom - margin.top]);
-                //define the x axis
-                var xAxis = d3.axisBottom()
-                    .scale(x)
-                    .tickFormat(d3.timeFormat(tickFormat))
-                    .ticks(24);
-                //define the y axis
-                var yAxis = d3.axisLeft()
-                    .scale(y)
-                    .tickSize(0);
-                //define axis functions for creating grid lines
-                function makeHorizGridlines() {
-                    return d3.axisLeft(y);
-                }
-
-                function makeVertGridlines() {
-                    return d3.axisBottom(x);
-                }
                 //define the chart area
                 var svg = d3.select("body")
                     .append("svg")
@@ -72,16 +53,28 @@ app.directive('tasks', ['taskBars', function (taskBars, $scope) {
                     .attr("width", width + margin.left + margin.right)
                     .attr("height", height + margin.top + margin.bottom)
                     .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
+                //define the x axis
+                var xAxis = d3.axisBottom()
+                    .scale(x)
+                    .tickFormat(d3.timeFormat(tickFormat))
+                    .ticks(24);
+                //define axis functions for creating grid lines
+                function makeHorizGridlines() {
+                    return d3.axisLeft(y);
+                }
+
+                function makeVertGridlines() {
+                    return d3.axisBottom(x);
+                }
                 //attach the x and y axes
                 svg.append("g")
                     .attr("class", "x axis")
                     .attr("transform", "translate(0, " + (height - margin.top - margin.bottom) + ")")
                     .transition()
                     .call(xAxis);
-                svg.append("g")
-                    .attr("class", "y axis")
-                    .transition()
-                    .call(yAxis);
+                var yAxis = svg.append("g")
+                    .style("font", "13px sans-serif")
+                    .call(d3.axisLeft(y).tickSize(0));
                 //attach the grid lines
                 svg.append("g")
                     .attr("class", "gridH")
@@ -99,9 +92,9 @@ app.directive('tasks', ['taskBars', function (taskBars, $scope) {
                         .ticks(24)
                     );
                 //attach the bars for the planned task times
-                taskBars.addBars(svg, data, x, y, 5, 35, "midnightblue", "startPlan", "endPlan");
+                taskBars.addBars(svg, data, x, y, 10, 33, "#404040", "startPlan", "endPlan");
                 //attach bars for the actual task times
-                taskBars.addBars(svg, data, x, y, 60, 87, "darkred", "startActual", "endActual");
+                taskBars.addBars(svg, data, x, y, 65, 88, "black", "startActual", "endActual");
 
             });
         }
